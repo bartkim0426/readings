9a6ac3abe1658d4aecbf4f01a82d167a395bf0e6
bartkim0426 <bartkim0426@gmail.com>
2018-12-15 04:36:23 +0900

    DONE: 5장 사용자 입력 저장하기 완료 Files: *
    Why:
    *
    
    ETC:
    *

--- TDD-Web-Dev-Python/superlists/functional_test/test.py
+++ TDD-Web-Dev-Python/superlists/functional_test/test.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
 import unittest
 
@@ -13,6 +13,12 @@ class NewVisitorTest(unittest.TestCase):
     # 테스트 에러 발생되어도 tearDown 실행 (setUp에 exception 있는상황 제외하고)
     def tearDown(self):
         self.browser.quit()
+
+    # refactor: 중복되는 함수를 피하기 위한 helper 함수
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     # test_ 로 시작
     def test_can_start_a_list_and_retriev_it_later(self):
@@ -35,26 +41,16 @@ class NewVisitorTest(unittest.TestCase):
         # 엔터 치면 1: 공작깃털 사기 입력됨
         inputbox.send_keys(Keys.ENTER)
         self.browser.implicitly_wait(3)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        # self.assertTrue(
-        #     any(row.text == '1: 공작깃털 사기' for row in rows),
-        #     "신규 작업이 테이블에 표시되지 않는다"
-        # )
-        self.assertIn('1: 공작깃털 사기', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: 공작깃털 사기')
+        self.browser.implicitly_wait(3)
 
         # 여분의 텍스트 상자에 다시 입력
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('공작깃털로 그물만들기')
         inputbox.send_keys(Keys.ENTER)
         self.browser.implicitly_wait(3)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(
-            '2: 공작깃털로 그물만들기',
-            [row.text for row in rows]
-        )
+        self.check_for_row_in_list_table('2: 공작깃털로 그물만들기')
+        self.browser.implicitly_wait(3)
 
         self.fail('Finish the test!')
 
