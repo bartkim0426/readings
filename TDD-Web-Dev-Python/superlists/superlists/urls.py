320ee601f31e2f1f5424282de2c6aebf8246b955
bartkim0426 <bartkim0426@gmail.com>
2018-12-20 01:14:59 +0900

    TDD: divide urls to list app Files: *
    Why:
    *
    
    ETC:
    *

--- TDD-Web-Dev-Python/superlists/superlists/urls.py
+++ TDD-Web-Dev-Python/superlists/superlists/urls.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from lists.views import home_page, view_list, new_list, add_item
+from lists.views import home_page
 
 urlpatterns = [
     path('', home_page, name='home'),
-    path('lists/the-only-list-in-the-world/', view_list, name='view_list'),
-    path('lists/<int:list_id>/', view_list, name='view_list'),
-    path('lists/<int:list_id>/add_item', add_item, name='add_item'),
-    path('lists/new', new_list, name='new_list'),
+    path('lists/', include('lists.urls')),
     # path('admin/', admin.site.urls),
 ]
