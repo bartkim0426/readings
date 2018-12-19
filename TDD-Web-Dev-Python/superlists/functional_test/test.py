7df6e74fa8fe445b68dc399861ea864c153699a3
bartkim0426 <bartkim0426@gmail.com>
2018-12-19 23:15:15 +0900

    ADD : wait_for_page_load for selenium 3 Files: *
    Why:
    *
    
    ETC:
    *

--- TDD-Web-Dev-Python/superlists/functional_test/test.py
+++ TDD-Web-Dev-Python/superlists/functional_test/test.py
@@ -1,18 +1,27 @@
+from django.test import LiveServerTestCase
+from contextlib import contextmanager
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
-import unittest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import staleness_of
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     # test 시작전
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
 
     # test 시작후
     # 테스트 에러 발생되어도 tearDown 실행 (setUp에 exception 있는상황 제외하고)
     def tearDown(self):
         self.browser.quit()
+
+    @contextmanager
+    def wait_for_page_load(self, timeout=30):
+        old_page = self.browser.find_element_by_tag_name("html")
+        yield WebDriverWait(self.browser, timeout).until(
+            staleness_of(old_page)
+        )
 
     # refactor: 중복되는 함수를 피하기 위한 helper 함수
     def check_for_row_in_list_table(self, row_text):
@@ -22,39 +31,32 @@ class NewVisitorTest(unittest.TestCase):
 
     # test_ 로 시작
     def test_can_start_a_list_and_retriev_it_later(self):
-        self.browser.get('http://localhost:8003')
+        self.browser.get(self.live_server_url)
 
         self.assertIn('To-Do', self.browser.title)
 
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
         # item 추가하기
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = lambda: self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholder'),
+            inputbox().get_attribute('placeholder'),
             '작업 아이템 입력'
         )
         
         # 공작깃털 사기 입력
-        inputbox.send_keys('공작깃털 사기')
+        inputbox().send_keys('공작깃털 사기')
 
         # 엔터 치면 1: 공작깃털 사기 입력됨
-        inputbox.send_keys(Keys.ENTER)
-        self.browser.implicitly_wait(3)
-        self.check_for_row_in_list_table('1: 공작깃털 사기')
-        self.browser.implicitly_wait(3)
+        inputbox().send_keys(Keys.ENTER)
+        with self.wait_for_page_load(timeout=10):
+            self.check_for_row_in_list_table('1: 공작깃털 사기')
 
         # 여분의 텍스트 상자에 다시 입력
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('공작깃털로 그물만들기')
-        inputbox.send_keys(Keys.ENTER)
-        self.browser.implicitly_wait(3)
-        self.check_for_row_in_list_table('2: 공작깃털로 그물만들기')
-        self.browser.implicitly_wait(3)
+        inputbox = lambda: self.browser.find_element_by_id('id_new_item')
+        inputbox().send_keys('공작깃털로 그물만들기')
+        inputbox().send_keys(Keys.ENTER)
+        with self.wait_for_page_load(timeout=10):
+            self.check_for_row_in_list_table('2: 공작깃털로 그물만들기')
 
         self.fail('Finish the test!')
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
-
